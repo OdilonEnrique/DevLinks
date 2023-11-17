9f0f65028b8c03b2f36d996135d9a8a8f3da6575
Odilon Enrique <odilonegduarte@gmail.com>
2023-11-17 13:53:23 -0300

    add script

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+  
+  // Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // Substituir a imagem
+  if (html.classList.contains("light")) {
+    // Se estiver light mode, adicionar a imagam light
+    img.setAttribute("src", "./img/avatar-light.png")
+  }else{
+    // Se estiver sem light mode, adicionar a normal
+    img.setAttribute("src", "./img/avatar.png")
+  }
+}
